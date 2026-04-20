029f96de0e5117d3d200234fb70a19c450a0a0ad
luishlb <luishlbarreto@gmail.com>
2026-04-20 13:29:30 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'offshore-scale-v1';
+
+const ARQUIVOS = [
+  '/',
+  '/index.html',
+  '/calendario.html',
+  '/style.css',
+  '/calendario.css',
+  '/calculos.js',
+  '/cal-embarque.js',
+  '/cal-consulta.js',
+  '/calculo.js',
+  '/perfis.js',
+  '/consulta.js',
+  '/calendario.js',
+  '/manifest.json',
+  '/icon.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ARQUIVOS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
